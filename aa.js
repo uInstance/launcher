e847d708328bd75872b2ad982693425e036df1e8
UTrosh <73399986+UTrosh@users.noreply.github.com>
2023-10-24 15:12:00 +0200

    upd

---
+++ aa.js
@@ -0,0 +1,20 @@
+const colors = require('colors')
+
+console.log(`nScan result :`)
+console.log("Service Type : Minecraft Modded server")
+console.log("=========================================")
+console.log("Server Information : Mohist 1.20.1.47.57")
+console.log("Server's plugin : LuckPerms, SkinsRestorer, OpeNLogin, PlaceholderAPI, CMILib, Vault, LPC, WorldEdit, DecentHolograms, XConomy, ODailyQuests, WorldGuard, Skript, Jobs, SkQuery, Mohist, Mohist")
+console.log("=== FR:FR")
+console.log("High Security:")
+console.log(`
+ - @CMILib(Crasher)@P50 Packet Netty
+ - @Skript(Crasher) Move Packet Crasher
+ - @DDoS(DDoS)@>--3 Not Reverse Proxied`.red)
+console.log(" ")
+console.log(`Medium Security:
+
+ - @SkinRestorer(Packet) Faking Skin
+ - @DDoS(Port)<>-2 Nmap Availible (101)`.yellow)
+console.log("")
+console.log("If you need help, contact nScan support")
